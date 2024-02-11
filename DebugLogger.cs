7dc3fefd607bc20e00e9607bbf88c2e0808c1188
Johannes Christel <johannes.christel.2004@gmail.com>
2024-02-11 17:58:55 +0100

    Rounded off DebugLogger.cs
    - Added documentation
    - Renamed variables to more speaking names
    - Removed the "." from standard file extension string

--- DebugLogger.cs
+++ DebugLogger.cs
@@ -4,13 +4,13 @@ using System.Text.RegularExpressions;
 namespace Helper.Logging
 {
   /// <summary>
-  /// This Class is meant to be a simple logging tool, which offers static and instance oriented methods of logging.<br/><br/>
-  /// You can either create one or multiple instances of this class, to write logs into either one single or multiple diffrent logfiles, scattered in diffrent directories.<br/>
+  /// This Class is meant to be a simple logging tool. It offers static and instance oriented methods of logging.<br/><br/>
+  /// You can create one or multiple instances of this class, to write logs into either one single or multiple diffrent logfiles, possibly scattered in diffrent directories.<br/>
   /// To keep track of your diffrent logger instances, you can name them.<br/>
   /// -> They will then mark every log entry with their name.<br/>
   /// This is useful when tracking multiple points of interest in a single application, like different internal workers or API's<br/><br/>
   /// 
-  /// The simpler approach to this is the static <see cref="QuickLog(string, string)"/> method, which enables much simpler single line logpoints.
+  /// The simpler approach to this is the static method <see cref="QuickLog(string, string)"/> method, which enables much simpler single line logpoints.
   /// </summary>
   public class DebugLogger
   {
@@ -20,17 +20,17 @@ namespace Helper.Logging
 
     private const string _StandardFileName = @"debug";
 
-    private const string _StandardFileExtension = @".txt";
+    private const string _StandardFileExtension = @"txt";
 
-    public string FullFileName;
+    public string AbsoluteFilePath;
 
     public string LoggerName = "UnnamedLogger";
 
     public bool TimeStampInLogfileName = true;
 
-    public DebugLogger(string fullFilePath, string loggerName = "", bool timeStampInLogfileName = true)
+    public DebugLogger(string absoluteFilePath, string loggerName = "", bool timeStampInLogfileName = true)
     {
-      FullFileName = fullFilePath;
+      AbsoluteFilePath = absoluteFilePath;
       LoggerName = loggerName != "" ? loggerName : LoggerName;
       TimeStampInLogfileName = timeStampInLogfileName;
     }
@@ -43,7 +43,7 @@ namespace Helper.Logging
     /// <param name="logContent"></param>
     public void Log(string logContent = "")
     {
-      Log(fullFilePath: FullFileName, loggerName: LoggerName, timeStampInLogfileName: TimeStampInLogfileName, logContent: logContent);
+      Log(absoluteFilePath: AbsoluteFilePath, loggerName: LoggerName, timeStampInLogfileName: TimeStampInLogfileName, logContent: logContent);
     }
 
     /// <summary>
@@ -51,24 +51,24 @@ namespace Helper.Logging
     /// Throws <see cref="Exception"/> when necessary file-system-permissions are not in place<br/>
     /// Or an <see cref="ArgumentException"/> if the submitted filepath is invalid
     /// </summary>
-    /// <param name="fullFilePath"></param>
+    /// <param name="absoluteFilePath"></param>
     /// <param name="loggerName"></param>
     /// <param name="timeStampInLogfileName"></param>
     /// <param name="logContent"></param>
     /// <exception cref="Exception"></exception>
     /// <exception cref="ArgumentException"></exception>
-    private void Log(string fullFilePath, string loggerName, bool timeStampInLogfileName, string logContent = "")
+    private void Log(string absoluteFilePath, string loggerName, bool timeStampInLogfileName, string logContent = "")
     {
-      if (!Path.IsPathFullyQualified((ReadOnlySpan<char>)fullFilePath))
+      if (!Path.IsPathFullyQualified((ReadOnlySpan<char>)absoluteFilePath))
         throw new ArgumentException("Logging exception: Invalid filepath");
 
-      if (!Path.Exists(Path.GetDirectoryName(fullFilePath)))
-        Directory.CreateDirectory(Path.GetDirectoryName(fullFilePath));
+      if (!Path.Exists(Path.GetDirectoryName(absoluteFilePath)))
+        Directory.CreateDirectory(Path.GetDirectoryName(absoluteFilePath));
 
       if (timeStampInLogfileName)
-        fullFilePath = Path.GetDirectoryName(fullFilePath) + @"\" + Path.GetFileNameWithoutExtension(fullFilePath) + DateTime.Now.ToString("-yyyy-MM-dd_HH-mm-ss") + Path.GetExtension(fullFilePath);
+        absoluteFilePath = Path.GetDirectoryName(absoluteFilePath) + @"\" + Path.GetFileNameWithoutExtension(absoluteFilePath) + DateTime.Now.ToString("-yyyy-MM-dd_HH-mm-ss") + Path.GetExtension(absoluteFilePath);
 
-      using (var file = new FileStream(fullFilePath, FileMode.Append, FileAccess.Write))
+      using (var file = new FileStream(absoluteFilePath, FileMode.Append, FileAccess.Write))
       {
         if (!file.CanWrite)
           throw new Exception("Logging exception: No append permission");
@@ -95,13 +95,13 @@ namespace Helper.Logging
     /// Enables a quick simple logging functionality, with the Help of standard filepaths if none get supplied.<br/>
     /// </summary>
     /// <remarks>
-    /// The default value for thre parameter <paramref name="fullFilePath"/> is currently faulty.<br/>
+    /// The default value for thre parameter <paramref name="absoluteFilePath"/> is currently faulty.<br/>
     /// Of course there is always only one "\" at a time.
     /// </remarks>
-    public static void QuickLog(string logContent = "", string fullFilePath = _StandardFilePath + _StandardFileName + _StandardFileExtension)
+    public static void QuickLog(string logContent = "", string absoluteFilePath = _StandardFilePath + _StandardFileName + "." + _StandardFileExtension)
 
     {
-      var logger = new DebugLogger(fullFilePath: fullFilePath, loggerName: _QuickLoggerName, timeStampInLogfileName: true);
+      var logger = new DebugLogger(absoluteFilePath: absoluteFilePath, loggerName: _QuickLoggerName, timeStampInLogfileName: true);
 
       Log(logger, logContent);
     }
@@ -112,7 +112,7 @@ namespace Helper.Logging
     /// </summary>
     public static void QuickLog(string filePath, string fileName, string fileExtension, string logContent = "")
     {
-      QuickLog(logContent: logContent, fullFilePath: filePath + fileName + fileExtension);
+      QuickLog(logContent: logContent, absoluteFilePath: filePath + fileName + fileExtension);
     }
 
     /// <summary>
@@ -120,7 +120,7 @@ namespace Helper.Logging
     /// </summary>
     private static void Log(DebugLogger logger, string logContent = "")
     {
-      logger.Log(fullFilePath: logger.FullFileName, loggerName: logger.LoggerName, timeStampInLogfileName: logger.TimeStampInLogfileName, logContent: logContent);
+      logger.Log(absoluteFilePath: logger.AbsoluteFilePath, loggerName: logger.LoggerName, timeStampInLogfileName: logger.TimeStampInLogfileName, logContent: logContent);
     }
 
     #endregion Static functionallity
