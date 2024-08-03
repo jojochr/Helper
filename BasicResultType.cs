803864b8d4b308617d5bc9da73b2ba656b588981
jojochr <jojochr@users.noreply.github.com>
2024-08-03 17:02:57 +0200

    Corrected the indenting of result type

--- BasicResultType.cs
+++ BasicResultType.cs
@@ -3,39 +3,39 @@
 /// Please either look for a Result-implementation with more Features like Fluent.Results if needed or, if you really want to, extend this type.
 /// </summary>
 public readonly struct Result<T, E> {
-        private readonly bool _success;
-        public readonly T Value;
-        public readonly E Error;
+    private readonly bool _success;
+    public readonly T Value;
+    public readonly E Error;
 
-        private Result(T v, E e, bool success)
-        {
-            Value = v;
-            Error = e;
-            _success = success;
-        }
+    private Result(T v, E e, bool success)
+    {
+        Value = v;
+        Error = e;
+        _success = success;
+    }
 
-        public bool IsOk => _success;
+    public bool IsOk => _success;
 
-        public static Result<T, E> Ok(T v)
-        {
-            return new(v, default(E), true);
-        }
+    public static Result<T, E> Ok(T v)
+    {
+        return new(v, default(E), true);
+    }
 
-        public static Result<T, E> Err(E e)
-        {
-            return new(default(T), e, false);
-        }
+    public static Result<T, E> Err(E e)
+    {
+        return new(default(T), e, false);
+    }
 
-        public static implicit operator Result<T, E>(T v) => new(v, default(E), true);
-        public static implicit operator Result<T, E>(E e) => new(default(T), e, false);
+    public static implicit operator Result<T, E>(T v) => new(v, default(E), true);
+    public static implicit operator Result<T, E>(E e) => new(default(T), e, false);
 
-        /// <summary>
-        /// This can be used to handle both the success case and the error case and output a specified type
-        /// </summary>
-        public R Match<R>(
-                Func<T, R> success,
-                Func<E, R> failure) =>
-            _success ? success(Value) : failure(Error);
+    /// <summary>
+    /// This can be used to handle both the success case and the error case and output a specified type
+    /// </summary>
+    public R Match<R>(
+            Func<T, R> success,
+            Func<E, R> failure) =>
+        _success ? success(Value) : failure(Error);
     
     /// <summary>
     /// This can be used to transform the values inside a result without resolving it.
